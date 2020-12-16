50faf74ac0ae5bb4b8adc971297aadaac8e1140c
Philippe Baumann <36074902+PhilCPNV@users.noreply.github.com>
2020-12-16 10:53:48 +0100

    Adding Dogs Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ConsultationReportComponent } from './consultation-report/consultation-report.component';
+import { DogsComponent } from './dogs/dogs.component';
 
 
 const routes: Routes = [
@@ -18,7 +20,8 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    ConsultationReportComponent
+    ConsultationReportComponent,
+    DogsComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +30,8 @@ const routes: Routes = [
     MatSidenavModule,
     MatListModule,
     MatIconModule,
-    RouterModule.forRoot(routes)
+    FormsModule,
+    RouterModule.forRoot(routes),
   ],
   exports: [ RouterModule ],
   providers: [],
