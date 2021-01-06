3f7f1c0b6cd203ed757871ac651a5031cc01fa05
Noe <Noe.BERDOZ@cpnv.ch>
2021-01-06 12:05:15 +0100

    Added table with filter input and hardcode data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import {MatButtonModule} from '@angular/material/button';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -38,6 +39,7 @@ const routes: Routes = [
     MatButtonModule,
     MatAutocompleteModule,
     MatInputModule,
+    MatTableModule,
     RouterModule.forRoot(routes)
   ],
   exports: [ RouterModule ],
