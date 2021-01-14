9f51783bbe2adab0c55e9dab206a613568eaf106
Philippe B <36074902+PhilCPNV@users.noreply.github.com>
2021-01-14 16:59:55 +0100

    Update Dogs Service
    Finish implementation of  API calls

--- src/app/dogs/dogs.service.ts
+++ src/app/dogs/dogs.service.ts
@@ -2,17 +2,37 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 
+const dogsUrl = 'http://localhost:3000/dogs';
+
 @Injectable({
   providedIn: 'root'
 })
 
 export class DogsService {
 
-  dogsUrl = 'http://localhost:3000/dogs';
-
   constructor(private http: HttpClient) { }
 
   getDogs(): Observable<[]> {
-    return this.http.get<[]>(this.dogsUrl);
+    return this.http.get<[]>(dogsUrl);
+  }
+
+  getDog(id: number): Observable<any> {
+    return this.http.get(`${dogsUrl}/${id}`)
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(dogsUrl, data);
+  }
+
+  update(id: number, data: any): Observable<any> {
+    return this.http.put(`${dogsUrl}/${id}`, data);
+  }
+
+  findByName(name: string): Observable<any> {
+    return this.http.get(`${dogsUrl}?title=${name}`);
+  }
+
+  delete(id: number) : Observable<any> {
+    return this.http.delete(`${dogsUrl}/${id}`);
   }
 }
